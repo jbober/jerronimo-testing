54b13b2c3b371ef287cba7bb7a2da77d6d40ace1
jaroslaw.bober@gmail.com <jaroslaw.bober@gmail.com@78b186b2-170a-b7b9-3e36-5440a593a672>
2012-08-23 22:20:24 +0000

    getting used to it :)

--- trunk/test/example.cpp
+++ trunk/test/example.cpp
@@ -2,7 +2,9 @@
 TEST(example, Zero) {
   EXPECT_EQ(2+2, 4);
 }
-//TEST(example2, failing_test) {
-//  EXPECT_EQ(2+2, 5);
-//}
-
+TEST(failed_test, failing_test) {
+  EXPECT_EQ(2+3, 7);
+}
+TEST(example, anotherOne){
+	EXPECT_EQ(2,2);
+	}
---
+++ trunk/test/example2.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+TEST(example3, Zero) {
+  EXPECT_EQ(2+2, 4);
+}
+TEST(example4, failing_test) {
+  EXPECT_EQ(2+3, 5);
+}
