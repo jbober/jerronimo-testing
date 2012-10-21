7981d9ed3b0b935fbf646beb90e0353e2209fc93
jaroslaw.bober@gmail.com <jaroslaw.bober@gmail.com@78b186b2-170a-b7b9-3e36-5440a593a672>
2012-10-21 17:47:07 +0000

    adding sqlite

--- trunk/test/example.cpp
+++ trunk/test/example.cpp
@@ -1,14 +1,12 @@
 #include "gtest/gtest.h"
-#include "unresolved_symbol.h"
+#include "sqlite_poligon.h"
 
-TEST(example, Zero) {
+TEST(reality_check, test) {
   EXPECT_EQ(2+2, 4);
 }
-TEST(failed_test, fixed_test) {
-  EXPECT_EQ(2+3, 5);
+
+TEST(sqliteTest, openDatabase)
+{
+	Database db;
+	db.openDatabase();
 }
-TEST(example, anotherOne){
-	UnresolvedSymbol unresolvedSymbol;
-	//unresolvedSymbol.unresolvedMethod();
-	EXPECT_EQ(5, unresolvedSymbol.definedMethod());
-	}
